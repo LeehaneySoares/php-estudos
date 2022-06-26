f319245ac5fa3524e8c09c1e8fa2ba0dbe27b517
Leehaney Soares Ferreira Nazaré <leehaney.soares@gmail.com>
2022-06-26 19:53:42 -0300

    feat(php): aprendendo os tipos dinâmicos de variáveis, o variable variables

---
+++ VariableVariables/index.php
@@ -0,0 +1,26 @@
+<?php
+  #Variable Variables
+
+  // $leehaney = 'nome';
+  // $$leehaney = 'Leehaney';
+
+  // print_r($nome);
+
+  #Refenciando variáveis
+
+  // $a = 5;
+  // $b = &$a;
+  // $a = TRUE;
+
+  // echo 'TRUE'
+
+  #Booleans
+
+  $umidade = 40;
+
+  $vai_chover = ($umidade > 90);
+
+  if ($vai_chover) {
+    echo "Está chovendo!";
+  }
+?>
