0e178c732625fa5dd855b509aff3e95e7d1308b8
Leehaney Soares Ferreira Nazaré <leehaney.soares@gmail.com>
2022-06-26 19:23:49 -0300

    chore(aula2): Aprendendo um pouco sobre o var_dump e o print-r, a diferença entre eles

--- php2/index.php
+++ php2/index.php
@@ -18,22 +18,28 @@
  //Array Spread
  //Similiar com o que temos em Javascript 
 
-  $bolo1 = [
-    'açucar',
-    'farinha de trigo',
-    'ovo',
-    'leite',
-    'fermento em pó'
-  ];
+  // $bolo1 = [
+  //   'açucar',
+  //   'farinha de trigo',
+  //   'ovo',
+  //   'leite',
+  //   'fermento em pó'
+  // ];
 
-  $bolo2 = [
-    'Leehaney',
-    'Soares',
-    ...$bolo1,
-    'corante',
-    'farinha lacta'
-  ];
+  // $bolo2 = [
+  //   'Leehaney',
+  //   'Soares',
+  //   ...$bolo1,
+  //   'corante',
+  //   'farinha lacta'
+  // ];
 
-  print_r($bolo1)
+  // print_r($bolo1)
+
+  $nomes = array(25, 22, 48, 9, 47);
+
+  print_r($nomes);
+
+  var_dump($nomes);
 
 ?>
