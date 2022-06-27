2a217b6643dc46cf76ccf3f57c6d6862a255f98f
Leehaney Soares Ferreira Nazaré <leehaney.soares@gmail.com>
2022-06-27 16:01:51 -0300

    chore(php): inserindo comentários para exemplificar a direfença do require do include

--- include_required/index.php
+++ include_required/index.php
@@ -1,7 +1,14 @@
 <?php
 
-  #include é semelhante ao import
+  /**
+   *  TODO: include é semelhante ao import do JS. 
+   */
   include './biblioteca.php';
+  /**
+   * TODO: O require é semelhante ao include, diferença apenas na manipulação dos erros
+   * Enquanto o include lança um warning o require lança um FATAL ERROR;
+   */
+  require './biblioteca.php';
 
   echo 'Quadrado: '.quadrado(9).'<br/>';
   echo '<h1>'.message('Hello, world!').'</h1>'
