ea5ca52b112590268694e3516c8495d58a07bd79
Leehaney Soares <leehaney.soares@gmail.com>
2022-10-17 14:53:05 -0300

    chore(php): removendo o conceito de currying function

--- function/arrow-function/index.php
+++ function/arrow-function/index.php
@@ -4,17 +4,9 @@
   $multiplicar = fn(int $n1, int $n2): int => $n1 * $n2;
   $dividir = fn(int $n1, int $n2): int => $n1 / $n2;
 
-  /* 
-    echo "
-    Soma: ".$somar(5, 5)."</br>".
-    "Subtração: ".$sub(5, 5)."</br>".
-    "Multiplicação: ".$multiplicar(5, 5)."</br>".
-    "Divisão: ".$dividir(5, 5);
-  */
-
-  $response = fn($n1): mixed => fn($n2): int => $n1 * $n2; 
-
-  define("RESPONSE", $response(200)(300));
-
-  echo RESPONSE;
+  echo "
+  Soma: ".$somar(5, 5)."</br>".
+  "Subtração: ".$sub(5, 5)."</br>".
+  "Multiplicação: ".$multiplicar(5, 5)."</br>".
+  "Divisão: ".$dividir(5, 5);
 ?>
