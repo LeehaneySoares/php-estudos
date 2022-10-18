3ae2511507eb90a2d3f4946ee72c75ed2a033aa2
Leehaney Soares <leehaney.soares@gmail.com>
2022-10-17 23:49:30 -0300

    learn(php): aprendendo sobre funções nativas de string

---
+++ function/string-function/index.php
@@ -0,0 +1,16 @@
+<?php
+  $nomeSujo = '       Leehaney        ';
+  $nomeLimpo = trim($nomeSujo);
+  echo "Nome sujo: $nomeSujo ".strlen($nomeSujo)."</br>";
+  echo "Nome limpo: $nomeLimpo ".strlen($nomeLimpo)."</br>";
+
+  $nomeCaixaAlta = 'LEEHANEY';
+
+  echo strtolower($nomeCaixaAlta)."</br>";
+  echo strtoupper($nomeCaixaAlta)."</br>";
+
+  $dinheiro = 'R$ 15,00';
+
+  echo str_replace('R$', '', $dinheiro)."</br>";
+
+?>
