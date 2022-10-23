4b7b6e68a82c579c41e567f6ccecd9df0035335f
Leehaney Soares <leehaney.soares@gmail.com>
2022-10-23 20:58:36 -0300

    learn(php): comentando as funções utilizadas

--- function/arrays-functions/index.php
+++ function/arrays-functions/index.php
@@ -1,8 +1,13 @@
 <?php
+
+  // Contando quantos elementos possui um array
+
   $lista = ['Leehaney', 'Valmira', 'Betania', 'Taylor'];
   $quantity = count($lista);
 
   echo "O total de elementos no array é: $quantity".'</br>';
+
+  // Verificando se os elementos de um array é diferente de outro 
 
   $candidatos = ['Leehaney', 'Felipe', 'Lucas', 'Valmira', 'Rafael', 'Eduardo'];
   $aprovados = ['Leehaney', 'Valmira', 'Lucas'];
@@ -11,6 +16,8 @@
   echo "<h2>Candidatos que não foram aprovados: </h2>".'</br>';
   print_r($reprovados);
 
+  // Filtrando elementos de um array
+
   $numeros = [10, 24, 56, 34, 22, 60, 87, 76];
 
   $filtrados = array_filter($numeros, fn($item) => ($item < 38));
@@ -18,7 +25,36 @@
   echo "<h2>Os números sortiados foram: </h2>".'</br>';
   print_r($filtrados);
 
-  $teste = array_pop($numeros);
+  echo '</br>';
 
-  print_r($teste);
+  // Removendo o último elemento de um array
+
+  $teste1 = array_pop($numeros);
+  $teste2 = array_shift($numeros);
+
+  print_r($teste1);
+  print_r($teste2);
+
+  echo '</br>';
+
+  // Verificando existência de algum elemento no array
+
+  echo 'O valor 87 existe em meu array ? </br>';
+  print_r($numeros);
+  echo '</br>';
+
+  echo in_array(87, $numeros) ? 'Existe </br>' : 'NÃO existe </br>';
+
+  $nomes = ['Leehaney', 'João', 'Douglas', 'Erick', 'Leonardo'];
+
+  echo 'Existe o nome Douglas em minha lista ? </br>';
+
+  echo in_array('Maria', $nomes) ? 'Existe </br>' : 'Não existe </br>';
+
+  // Descobrindo a posição de um elemento no array
+
+  $searchName = 'Cleber';
+  $posicao = array_search($searchName, $nomes);
+
+  echo is_int($posicao) ? "O nome $searchName, está na posição: $posicao" : 'Nome não encontrado';
 ?>
