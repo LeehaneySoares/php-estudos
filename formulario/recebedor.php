57cf3890d06867275f097404270db37bae6125ba
Leehaney Soares <leehaney.soares@gmail.com>
2023-01-11 20:14:33 -0300

    learn(php): aprendendo sobre o filter em php

--- formulario/recebedor.php
+++ formulario/recebedor.php
@@ -4,11 +4,14 @@
   global $senha;
 
   $nome = filter_input(INPUT_POST, 'nome');
-  $email = filter_input(INPUT_POST, 'email');
-  $senha = filter_input(INPUT_POST, 'senha');
+  $email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
+  $senha = filter_input(INPUT_POST, 'senha', FILTER_SANITIZE_NUMBER_INT);
 
-  if (isset($nome) && isset($email) && isset($senha)) {
-    
+  if ($nome && $email) {
+    echo "Nome: $nome </br>";
+    echo "Email: $email </br>";
+    echo "Senha: $senha </br>";
+  } else {
     header('location: index.php');
     exit;
   }
