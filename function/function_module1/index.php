a19f8b0e208c9a31bdb37293ecf76333fe93a237
Leehaney Soares <leehaney.soares@gmail.com>
2023-01-18 21:49:30 -0300

    learn(php)

--- function/function_module1/index.php
+++ function/function_module1/index.php
@@ -21,15 +21,33 @@
 
   // Utilizando varáveis estáticas;
 
-  function percorre ($quilometro) {
-    static $total;
-    $total += $quilometro;
+  // function percorre ($quilometro) {
+  //   static $total;
+  //   $total += $quilometro;
 
-    echo "Percorreu mais $quilometro do total de $total </br>";
+  //   echo "Percorreu mais $quilometro do total de $total </br>";
+  // }
+
+  // percorre(100);
+  // percorre(250);
+  // percorre(1000);
+
+  // Trabalhando com passagem de parâmetros por valor e por referencia;
+
+  function passagem (&$referencia, $valor) {
+    echo "Esse argumento utiliza a passagem por referencia: $referencia </br>";
+    $referencia = 'Testando a referencia';
+    echo "Esse argumento é por valor: $valor </br>";
+    $valor = 5255;
   }
 
+  // parâmetros passados por referencia, caso tenha alguma alteração dentro da função,
+  // seu valor original será alterado também;
 
-  percorre(100);
-  percorre(250);
-  percorre(1000);
+  $nome = 'Leehaney';
+  $valor = 50;
+
+  passagem($nome, $valor);
+  echo "Esse é o parâmetro (referencia) modificado dentro da função: $nome </br>";
+  echo "Esse é o parâmetro (valor) modificado dentro da função: $valor </br>";
 ?>
