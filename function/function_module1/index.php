cc9eee2d2d7eab94f38f59f604c75f0552a96a62
Leehaney Soares <leehaney.soares@gmail.com>
2023-01-18 21:40:57 -0300

    learn(php): aprendendo a trabalhar com variáveis globais

--- function/function_module1/index.php
+++ function/function_module1/index.php
@@ -1,21 +1,35 @@
 <?php
-  function calcular_obesidade($peso, $altura) {
-    return $peso / ($altura ** 2);
-  };
+  // function calcular_obesidade($peso, $altura) {
+  //   return $peso / ($altura ** 2);
+  // };
 
-  $result = calcular_obesidade(80, 1.73);
+  // $result = calcular_obesidade(80, 1.73);
 
-  echo $result.'</br>';
+  // echo $result.'</br>';
 
-  function km2mi ($quilometro) { // Na primeira chamada $quilometro é = 100, e depois é = 200;
-    global $total; // a variável total está disponível apenas neste arquivo;
+  // Utilizando variáveis globais;
+
+  // function km2mi ($quilometro) { // Na primeira chamada $quilometro é = 100, e depois é = 200;
+  //   global $total; // a variável total está disponível apenas neste arquivo;
+  //   $total += $quilometro;
+  //   return $quilometro * 0.6;
+  // }
+
+  // echo 'percorreu'.PHP_EOL.km2mi(100).PHP_EOL.'milhas </br>';
+  // echo 'percorreu'.PHP_EOL.km2mi(200).PHP_EOL.'milhas </br>';
+  // echo 'percorreu no total'.PHP_EOL.$total.PHP_EOL.'quilometros </br>';
+
+  // Utilizando varáveis estáticas;
+
+  function percorre ($quilometro) {
+    static $total;
     $total += $quilometro;
-    return $quilometro * 0.6;
+
+    echo "Percorreu mais $quilometro do total de $total </br>";
   }
 
-  echo 'percorreu'.PHP_EOL.km2mi(100).PHP_EOL.'milhas </br>';
-  echo 'percorreu'.PHP_EOL.km2mi(200).PHP_EOL.'milhas </br>';
-  echo 'percorreu no total'.PHP_EOL.$total.PHP_EOL.'quilometros </br>';
 
-  header('location: teste.php');
+  percorre(100);
+  percorre(250);
+  percorre(1000);
 ?>
