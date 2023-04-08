6dac9e4d091ff03458048211bfa792715d066dab
Leehaney Soares <leehaney.soares@gmail.com>
2023-04-08 20:47:25 -0300

    learn(php): alterando o código para funcionar com functions anonymous

--- exercicios/exercicio7/tabuada.php
+++ exercicios/exercicio7/tabuada.php
@@ -2,13 +2,13 @@
   $value = $_POST['value'] ?? 0;
   $result;
 
-  function multiple (int $multiplicador, int $multiplicando): string {
+  $multiple = function (int $multiplicador, int $multiplicando): string {
     $result = $multiplicador * $multiplicando;
 
     return "$multiplicador x $multiplicando = $result <br/>";
-  }
+  };
 
   for ($i = 0; $i <= 10; $i++) {
-    echo multiple($value, $i);
+    echo $multiple($value, $i);
   }
 ?>
