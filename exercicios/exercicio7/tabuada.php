cb8f3e65219a8281163037f8aff6c2cb8bfc0916
Leehaney Soares <leehaney.soares@gmail.com>
2023-04-08 21:32:11 -0300

    refactor(php): removendo $value de global

--- exercicios/exercicio7/tabuada.php
+++ exercicios/exercicio7/tabuada.php
@@ -1,10 +1,9 @@
 <?php
-  $value = $_POST['value'] ?? 0;
-
+  
   function multiple ($multiplicando = 0) {
     if ($multiplicando > 10) return;
 
-    global $value;
+    $value = $_POST['value'] ?? 0;
     $result = $value * $multiplicando;
 
     echo "$value x $multiplicando = $result </br>";
