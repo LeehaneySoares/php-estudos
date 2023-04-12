a9d11135b05cdc2cdabd180690239eab43e9d2e5
Leehaney Soares <leehaney.soares@gmail.com>
2023-04-11 21:01:21 -0300

    learn(php): aprendendo como ler arquivo, abrir e fecha-los

--- archive_manipulation/index.php
+++ archive_manipulation/index.php
@@ -1,5 +1,21 @@
 <?php
-  $fp = fopen('./file.txt', 'r');
+  // $fb = fopen('file.txt', 'r'); // Abre o arquivo e retorna um identificador
 
-  echo $fp;
+  // if (feof($fb)) { // Verifica se o arquivo já foi lido, retorna TRUE se o ponteiro estiver no final e FALSE caso não tenha terminado
+  //   echo 'Cheguei ao fim';
+  // } else {
+  //   echo ' Não cheguei ao fim';
+  // }
+
+  // fclose($fb);
+
+  $file = fopen('file.txt', 'r'); // abrindo o arquivo
+
+  while (!feof($file)) { //verificando se o ponteiro chegou ao final
+    $buffer = fgets($file, 4096); //  lendo o arquivo
+
+    echo $buffer;
+  }
+
+  fclose($file); // fechando o arquivo, após aberto deve ser fechado!
 ?>
