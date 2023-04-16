26cc1c920e0166fcf1cf99f3dd3a02b47c6577e7
Leehaney Soares <leehaney.soares@gmail.com>
2023-04-16 20:14:45 -0300

    refactor(php): rmeovendo os comentários

--- archive_manipulation/index.php
+++ archive_manipulation/index.php
@@ -1,21 +1,10 @@
 <?php
-  // $fb = fopen('file.txt', 'r'); // Abre o arquivo e retorna um identificador
+  $file = fopen('file.txt', 'r');
 
-  // if (feof($fb)) { // Verifica se o arquivo já foi lido, retorna TRUE se o ponteiro estiver no final e FALSE caso não tenha terminado
-  //   echo 'Cheguei ao fim';
-  // } else {
-  //   echo ' Não cheguei ao fim';
-  // }
-
-  // fclose($fb);
-
-  $file = fopen('file.txt', 'r'); // abrindo o arquivo
-
-  while (!feof($file)) { //verificando se o ponteiro chegou ao final
-    $buffer = fgets($file, 4096); //  lendo o arquivo
-
+  while(!feof($file)) {
+    $buffer = fgets($file, 4029);
     echo $buffer;
   }
 
-  fclose($file); // fechando o arquivo, após aberto deve ser fechado!
+  fclose($file);
 ?>
